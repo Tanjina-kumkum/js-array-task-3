939d3b6bcdc93cdc1e079fce44824a1b957631ca
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-07-31 15:19:51 +0600

    includes js added

---
+++ books.js
@@ -0,0 +1,7 @@
+const books = ['bangla', 'english', 'mathematics', 'javascript', 'physics'];
+ const book=books.includes('javascript')
+console.log(books);
+console.log(book);
+console.log(books.includes('home economics'));
+
+
