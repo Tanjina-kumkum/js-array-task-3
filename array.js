732d670cacc488f7c3b411430d7025bfe55e0978
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-07-31 15:00:31 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,4 @@
+let fruits = ['mango', 'banana', 'apple', 'jackfruit', 'watermelon'];
+console.log(fruits[2]);
+fruits[2]= 'strawberry';
+console.log(fruits)
---
+++ tourist.js
@@ -0,0 +1,3 @@
+let touristPlace = ['cox bazar', 'madubkondo', 'kuakata'];
+touristPlace.push('shitakondo')
+console.log(touristPlace);
